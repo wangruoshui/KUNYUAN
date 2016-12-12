5973dff02f5011ef9e81e059f6446f619d8b2190
maxing1005 <1106501468@qq.com>
2016-12-12 23:04:38 +0800

    php修改过的前台页面，动态获取内容

--- src/back-end/kunyuan_v1.0/Application/Home/Controller/CompanyController.class.php
+++ src/back-end/kunyuan_v1.0/Application/Home/Controller/CompanyController.class.php
@@ -1,17 +1,44 @@
 <?php
-
+//author:maxing
 namespace Home\Controller;
+use Common\Controller\RestfulController;
 use Think\Controller;
 
 class CompanyController extends Controller
 {
+    protected $_db;
     protected function _initialize() {
         //启用layout二级布局
         layout("layout");
+        $this->_db=M('common');
     }
-    public function index()
-{
-    $this->display();
-}
+
+    /**
+     *
+     */
+    public function index(){
+        $results=array(
+        );
+        $result=$this->_db->where('kindid=4')->order('commonid desc')->limit(1)->select();
+        $results[]=$result;
+        $result=$this->_db->where('kindid=5')->order('commonid desc')->limit(1)->select();
+        $results[]=$result;
+        $result=$this->_db->where('kindid=6')->order('commonid desc')->limit(1)->select();
+        $results[]=$result;
+        $result=$this->_db->where('kindid=7')->order('commonid desc')->limit(1)->select();
+        $results[]=$result;
+        $result=$this->_db->where('kindid=8')->order('commonid desc')->limit(1)->select();
+        $results[]=$result;
+        $result=$this->_db->where('kindid=9')->order('commonid desc')->limit(1)->select();
+        $results[]=$result;
+        $result=$this->_db->where('kindid=10')->order('commonid desc')->limit(1)->select();
+        $results[]=$result;
+        $result=$this->_db->where('kindid=11')->order('commonid desc')->limit(1)->select();
+        $results[]=$result;
+//        dump($results);
+//        exit;
+        $this->assign("results",$results);
+       $this->display();
+    }
 
 }
--- src/back-end/kunyuan_v1.0/Application/Home/Controller/ContactusController.class.php
+++ src/back-end/kunyuan_v1.0/Application/Home/Controller/ContactusController.class.php
@@ -1,17 +1,39 @@
 <?php
 //前台页面----联系我们
+/*
+ * time:2016-12-7
+ * veson:v1.0
+ * ahthor:maxing
+*/
 namespace Home\Controller;
 use Think\Controller;
 
 class ContactusController extends Controller
 {
+    protected  $_db;
     protected function _initialize() {
         //启用layout二级布局
         layout("layout");
+        $this->_db=M("message");
     }
     public function index()
     {
         $this->display();
+    }
+    public function addmessage(){
+        $messages=I("post.");
+        $messages['time']=date("Y-m-d H:i:s",time());
+//        dump($messages);
+//        exit;
+        $result=$this->_db->add($messages);
+      //  dump($result);
+        if($result){
+            //插入成功
+            $this->success('提交成功,3秒后自动为您跳转到产品页面','../proTechnology/index',3);
+        }else{
+            //插入失败
+            $this->error('提交失败，3秒后自动跳回留言界面','../contactus/index',3);
+        }
     }
 
 }
