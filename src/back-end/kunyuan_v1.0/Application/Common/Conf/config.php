f403e743e006f3de46f21257651d38922c046059
maxing1005 <1106501468@qq.com>
2016-12-14 09:20:03 +0800

    数据库配置改回了localhost,并且在项目的上个一文件夹里有数据库最新版本

--- src/back-end/kunyuan_v1.0/Application/Common/Conf/config.php
+++ src/back-end/kunyuan_v1.0/Application/Common/Conf/config.php
@@ -44,7 +44,7 @@ return array(
 
     /* 数据库设置 */
     'DB_TYPE'                => 'mysql', // 数据库类型
-    'DB_HOST'                => '54.218.38.29', // 服务器地址
+    'DB_HOST'                => 'localhost', // 服务器地址
     'DB_NAME'                => 'kunyuan_v1.0', // 数据库名
     'DB_USER'                => 'root', // 用户名
     'DB_PWD'                 => '123456', // 密码
