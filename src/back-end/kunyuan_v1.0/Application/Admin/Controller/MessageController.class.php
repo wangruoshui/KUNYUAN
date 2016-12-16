a39e4898283f5b7a41008ad0f237898143ebee11
Wangxiaoman1 <279812881@qq.com>
2016-12-16 16:34:46 +0800

    王小曼的文件

--- src/back-end/kunyuan_v1.0/Application/Admin/Controller/MessageController.class.php
+++ src/back-end/kunyuan_v1.0/Application/Admin/Controller/MessageController.class.php
@@ -14,15 +14,87 @@ use Think\Controller;
 
 class MessageController extends Controller{
     public function index() {
-        $this->display();
+        if (isset($_SESSION['group_id'])){
+            //连接数据库
+            $message=M('message');
+            $message_list=$message->select();
+            //dump($ques_list);
+
+            //传递数据
+            $this->assign('message',$message_list);
+
+            //显示视图
+            $this->display();
+        }else{
+            exit('<script>top.location.href="/admin/log/login"</script>');
+            //$this->redirect('/admin/index/login', '', 0, '请登录!');
+        }
     }
     public function add(){
         $this->display();
     }
     public function manage(){
-        $this->display();
+        //获取数据
+        if (isset($_POST['submit'])) {
+            //dump(I('get.id'));
+            $condition = array();
+            if (isset($_POST['show_cover_pic'])){
+                $condition['status'] = $_POST['show_cover_pic'];
+            }else{
+                $condition['status'] = $_POST['show_cover_pic1'];
+            }
+
+
+            //连接数据库
+            $message = M('message');
+
+            $message_manage = $message->where("messageid='%s'",I('get.id'))->save($condition);
+            //dump($question_manage);
+            //判断是否添加成功
+            if ($message_manage) {
+                $this->redirect('admin/message/index', '', 0, '修改成功!');
+            } else {
+                $this->redirect('admin/message/index', '', 0, '修改失败!');
+            }
+
+            //$this->display('index');
+
+        }else{
+            //连接数据库
+            $message=M('message');
+            $message_list=$message->where('messageid=%d',I('get.id'))->select();
+            //dump($courier_list);
+
+            //传递数据
+            $this->assign('message',$message_list);
+
+            $this->display();
+        }
     }
     public function show(){
+        $message=M('message');
+        $message_list=$message->where('messageid=%d',I('get.id'))->select();
+        //dump($courier_list);
+
+        //传递数据
+        $this->assign('message',$message_list);
+
         $this->display();
+    }
+    public function del(){
+
+        $message = M('message');
+
+        $message_manage = $message->where("messageid='%s'",I('get.id'))->delete();
+        //dump($question_manage);
+        //判断是否添加成功
+        if ($message_manage) {
+            $this->redirect('admin/message/index', '', 0, '删除成功!');
+        } else {
+            $this->redirect('admin/message/index', '', 0, '删除失败!');
+        }
+
+        //$this->display('index');
+
     }
 }
